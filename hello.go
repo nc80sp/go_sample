7be3232fbb552bd895013f8b70a44c4ed315db92
nc80sp <nc80sp@gmail.com>
2023-06-21 12:47:13 +0000

    aaa

--- hello.go
+++ hello.go
@@ -15,43 +15,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func helloHandler2(w http.ResponseWriter, r *http.Request) {
-	hello := []byte('
-	<!doctype html>
-	<html dir="ltr" lang="ja">
-	  <head>
-		<meta charset="utf-8">
-		<title>新しいタブ</title>
-		<style>
-		  body {
-			background: #FFFFFF;
-			margin: 0;
-		  }
-	
-		  #backgroundImage {
-			border: none;
-			height: 100%;
-			pointer-events: none;
-			position: fixed;
-			top: 0;
-			visibility: hidden;
-			width: 100%;
-		  }
-	
-		  [show-background-image] #backgroundImage {
-			visibility: visible;
-		  }
-		</style>
-	  </head>
-	  <body>
-		<iframe id="backgroundImage" src=""></iframe>
-		<ntp-app></ntp-app>
-		<script type="module" src="new_tab_page.js"></script>
-		<link rel="stylesheet" href="chrome://resources/css/text_defaults_md.css">
-		<link rel="stylesheet" href="chrome://theme/colors.css?sets=ui,chrome">
-		<link rel="stylesheet" href="shared_vars.css">
-	  </body>
-	</html>
-	')
+	hello := []byte(`aaa`)
 	_, err := w.Write(hello)
 	if err != nil {
 		log.Fatal(err)
